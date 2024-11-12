9b6aaa6eb690fc3b7b61cef7c1302638127ba611
rezaulislam54 <rezaulislam5490@gmail.com>
2024-11-12 20:48:20 +0600

    all doctors page added and doctors filter

--- src/pages/doctors/Doctors.jsx
+++ src/pages/doctors/Doctors.jsx
@@ -1,10 +1,11 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { doctors } from "../../assets/assets_frontend/assets";
 import { useEffect, useState } from "react";
 
 const Doctors = () => {
   const { speciality } = useParams();
   const [filterData, setfilterData] = useState([]);
+  const navigate = useNavigate();
 
   const applyFilter = () => {
     if (speciality) {
@@ -23,22 +24,64 @@ const Doctors = () => {
       <h1 className="font-medium">Browse through the doctors specialist.</h1>
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-6 md:gap-5 mt-8">
         <div className="col-span-1 space-y-4">
-          <div className="border border-[#B4B4B4] rounded-md pl-3 py-1">
+          <div
+            onClick={() =>
+              speciality === "General physician"
+                ? navigate("/doctors")
+                : navigate("/doctors/General physician")
+            }
+            className="border border-[#B4B4B4] rounded-md pl-3 py-1 cursor-pointer"
+          >
             General physician
           </div>
-          <div className="border border-[#B4B4B4] rounded-md pl-3 py-1">
+          <div
+            onClick={() =>
+              speciality === "Gynecologist"
+                ? navigate("/doctors")
+                : navigate("/doctors/Gynecologist")
+            }
+            className="border border-[#B4B4B4] rounded-md pl-3 py-1 cursor-pointer"
+          >
             Gynecologist
           </div>
-          <div className="border border-[#B4B4B4] rounded-md pl-3 py-1">
+          <div
+            onClick={() =>
+              speciality === "Dermatologist"
+                ? navigate("/doctors")
+                : navigate("/doctors/Dermatologist")
+            }
+            className="border border-[#B4B4B4] rounded-md pl-3 py-1 cursor-pointer"
+          >
             Dermatologist
           </div>
-          <div className="border border-[#B4B4B4] rounded-md pl-3 py-1">
+          <div
+            onClick={() =>
+              speciality === "Pediatricians"
+                ? navigate("/doctors")
+                : navigate("/doctors/Pediatricians")
+            }
+            className="border border-[#B4B4B4] rounded-md pl-3 py-1 cursor-pointer"
+          >
             Pediatricians
           </div>
-          <div className="border border-[#B4B4B4] rounded-md pl-3 py-1">
+          <div
+            onClick={() =>
+              speciality === "Neurologist"
+                ? navigate("/doctors")
+                : navigate("/doctors/Neurologist")
+            }
+            className="border border-[#B4B4B4] rounded-md pl-3 py-1 cursor-pointer"
+          >
             Neurologist
           </div>
-          <div className="border border-[#B4B4B4] rounded-md pl-3 py-1">
+          <div
+            onClick={() =>
+              speciality === "Gastroenterologist"
+                ? navigate("/doctors")
+                : navigate("/doctors/Gastroenterologist")
+            }
+            className="border border-[#B4B4B4] rounded-md pl-3 py-1 cursor-pointer"
+          >
             Gastroenterologist
           </div>
         </div>
